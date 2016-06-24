bbf369532a53ca933e9d5d522d54c68328b92321
Richard Willars <richard@richardwilllars.com>
2016-06-24 12:18:34 +0100

    Syntax formatting

--- app.js
+++ app.js
@@ -11,146 +11,157 @@ mongoose.connect('mongodb://localhost/homebox');
 var models = require('./models');
 
 
-function doesDriverExist(driverId,type) {
-	return new Promise(function(resolve,reject) {
-		try {
-		    var driver = require('./drivers/homebox-driver-'+driverId);
-		    if(driver.type!==type) {
-		    	return resolve(false);
-		    }
-		    resolve(true);
-		}
-		catch (ex) {
-			resolve(false);
-		}
-	});
+function doesDriverExist(driverId, type) {
+  return new Promise(function(resolve, reject) {
+    try {
+      var driver = require('./drivers/homebox-driver-' + driverId);
+      if (driver.type !== type) {
+        return resolve(false);
+      }
+      resolve(true);
+    } catch (ex) {
+      resolve(false);
+    }
+  });
 }
 
 function loadDriver(driverId) {
-	return new Promise(function(resolve,reject) {
-		try {
-		    var driver = require('./drivers/homebox-driver-'+driverId);
-		    resolve(driver);
-		}
-		catch (ex) {
-			resolve(null);
-		}
-	});
+  return new Promise(function(resolve, reject) {
+    try {
+      var driver = require('./drivers/homebox-driver-' + driverId);
+      resolve(driver);
+    } catch (ex) {
+      resolve(null);
+    }
+  });
 }
 
-function createDevice(type,driver,deviceSpecs) {
-	var deviceSpecsObj = new models[type](deviceSpecs);
-	//var deviceSpecsObj = new Speaker(deviceSpecs);
+function createDevice(type, driver, deviceSpecs) {
+  var deviceSpecsObj = new models[type](deviceSpecs);
+  //var deviceSpecsObj = new Speaker(deviceSpecs);
 
-    return deviceSpecsObj.validate()
+  return deviceSpecsObj.validate()
     .then(function(validationFailed) {
-    	if(validationFailed) {
-    		throw new Error(validationFailed);
-    	}
+      if (validationFailed) {
+        throw new Error(validationFailed);
+      }
 
-    	var deviceObj = new models['device']({
-    		_id: md5(type+driver+deviceSpecsObj.deviceId),
-			type: type,
-			driver: driver,
-			specs: deviceSpecsObj
-		});
-    	return deviceObj.save();
+      var deviceObj = new models['device']({
+        _id: md5(type + driver + deviceSpecsObj.deviceId),
+        type: type,
+        driver: driver,
+        specs: deviceSpecsObj
+      });
+      return deviceObj.save();
     });
 }
 
-function updateDevice(device,specs) {
-	var deviceSpecsObj = new models[device.type](specs);
+function updateDevice(device, specs) {
+  var deviceSpecsObj = new models[device.type](specs);
 
-    return deviceSpecsObj.validate()
+  return deviceSpecsObj.validate()
     .then(function(validationFailed) {
-    	if(validationFailed) {
-    		throw new Error(validationFailed);
-    	}
-    	device.specs = specs;
-    	return device.save();
+      if (validationFailed) {
+        throw new Error(validationFailed);
+      }
+      device.specs = specs;
+      return device.save();
     });
 }
 
 // GET discover/speaker/sonos
-app.get('/discover/:type/:driver', function(req,res) {
+app.get('/discover/:type/:driver', function(req, res) {
   //check that the driver exists and that it matches the specified type
   var foundDevices = [];
   var existingDevices = [];
 
-  doesDriverExist(req.params.driver,req.params.type)
-  	.then(function(foundDriver) {
-  		//if found, load it
-  		if(foundDriver===false) {
-  			throw new Error(404,'driver not found');
-  		}
-  		return loadDriver(req.params.driver);
-  	})
-  	.then(function(driver) {
-  		//call the discover method on the driver and wait for it to return devices
-  		return driver.discover();	
-  	})
-  	.then(function(devices) {
-  		foundDevices = devices;
-  		//get a list of existing devices from the db
-  		return models['device'].find({ type: req.params.type, driver: req.params.driver }).exec();
-  	})
-  	.then(function(existingDevicesArr) {
-  		existingDevices = existingDevicesArr;
+  doesDriverExist(req.params.driver, req.params.type)
+    .then(function(foundDriver) {
+      //if found, load it
+      if (foundDriver === false) {
+        throw new Error(404, 'driver not found');
+      }
+      return loadDriver(req.params.driver);
+    })
+    .then(function(driver) {
+      //call the discover method on the driver and wait for it to return devices
+      return driver.discover();
+    })
+    .then(function(devices) {
+      foundDevices = devices;
+      //get a list of existing devices from the db
+      return models['device'].find({
+        type: req.params.type,
+        driver: req.params.driver
+      }).exec();
+    })
+    .then(function(existingDevicesArr) {
+      existingDevices = existingDevicesArr;
 
-  		//loop through existingDevices and determine if they exist in the discovery list
-  		var toUpdate = [];
-  		_.filter(existingDevices, function(obj){
-  			return _.find(foundDevices, function(obj2){
-  				if(obj._id===md5(req.params.type+req.params.driver+obj2.deviceId)) {
-  					toUpdate.push({ device: obj, specs: obj2});
-  					return true;
-  				}
-  				return false;
-  			});
-  		});
-  		//if they do exist in the discovery list, update them
-  		var promises = [];
-  		for(var i in toUpdate) {
-  			promises.push(updateDevice(toUpdate[i].device,toUpdate[i].specs));
-  		}
-  		return Promise.all(promises);
-  	}).then(function() {
-  		//loop through existingDevices and determine if they don't exist in the discovery list
-  		var noLongerExists = _.filter(existingDevices, function(obj){
-  			return !_.find(foundDevices, function(obj2){
-  				return obj._id===md5(req.params.type+req.params.driver+obj2.deviceId);
-  			});
-  		});
-  		//if they don't exist in the discovery list, delete them
-  		if(noLongerExists.length===0) {
-  			return;
-  		}
-  		var noLongerExistsIds = _.pluck(noLongerExists, '_id');
-  		return models['device'].remove({ _id: { $in: noLongerExistsIds } }).exec();
-  	}).then(function() {
-  		//loop through foundDevices and determine if they don't exist in the discovery list
-  		var newDevices = _.filter(foundDevices, function(obj){
-  			return !_.find(existingDevices, function(obj2){
-  				return obj2._id===md5(req.params.type+req.params.driver+obj.deviceId);
-  			});
-  		});
-  		//if there are any other devices in discovery list, create them
-  		var promises = [];
-  		for(var i in newDevices) {
-  			promises.push(createDevice(req.params.type,req.params.driver,newDevices[i]));
-  		}
-  		return Promise.all(promises);
-  	})
-  	.then(function() {
-  		//get the entire list of devices from the db
-  		return models['device'].find({ type: req.params.type, driver: req.params.driver }).exec();
-  	})
-  	.then(function(devices) {
-  		res.json(devices);
-  	})
-  	.catch(function(e) {
-  		console.log(e);
-  	});
+      //loop through existingDevices and determine if they exist in the discovery list
+      var toUpdate = [];
+      _.filter(existingDevices, function(obj) {
+        return _.find(foundDevices, function(obj2) {
+          if (obj._id === md5(req.params.type + req.params.driver + obj2.deviceId)) {
+            toUpdate.push({
+              device: obj,
+              specs: obj2
+            });
+            return true;
+          }
+          return false;
+        });
+      });
+      //if they do exist in the discovery list, update them
+      var promises = [];
+      for (var i in toUpdate) {
+        promises.push(updateDevice(toUpdate[i].device, toUpdate[i].specs));
+      }
+      return Promise.all(promises);
+    }).then(function() {
+      //loop through existingDevices and determine if they don't exist in the discovery list
+      var noLongerExists = _.filter(existingDevices, function(obj) {
+        return !_.find(foundDevices, function(obj2) {
+          return obj._id === md5(req.params.type + req.params.driver + obj2.deviceId);
+        });
+      });
+      //if they don't exist in the discovery list, delete them
+      if (noLongerExists.length === 0) {
+        return;
+      }
+      var noLongerExistsIds = _.pluck(noLongerExists, '_id');
+      return models['device'].remove({
+        _id: {
+          $in: noLongerExistsIds
+        }
+      }).exec();
+    }).then(function() {
+      //loop through foundDevices and determine if they don't exist in the discovery list
+      var newDevices = _.filter(foundDevices, function(obj) {
+        return !_.find(existingDevices, function(obj2) {
+          return obj2._id === md5(req.params.type + req.params.driver + obj.deviceId);
+        });
+      });
+      //if there are any other devices in discovery list, create them
+      var promises = [];
+      for (var i in newDevices) {
+        promises.push(createDevice(req.params.type, req.params.driver, newDevices[i]));
+      }
+      return Promise.all(promises);
+    })
+    .then(function() {
+      //get the entire list of devices from the db
+      return models['device'].find({
+        type: req.params.type,
+        driver: req.params.driver
+      }).exec();
+    })
+    .then(function(devices) {
+      res.json(devices);
+    })
+    .catch(function(e) {
+      console.log(e);
+    });
 });
 
 
@@ -158,88 +169,99 @@ app.get('/discover/:type/:driver', function(req,res) {
 GET devices
 -> GET devices
 */
-app.get('/devices/', function(req,res) {
-	return models['device'].find().exec()
-	.then(function(devices) {
-		res.json(devices);
-	});
+app.get('/devices/', function(req, res) {
+  return models['device'].find().exec()
+    .then(function(devices) {
+      res.json(devices);
+    });
 });
 
 /*
 GET devices/:type
 -> GET devices/speaker
 */
-app.get('/devices/:type', function(req,res) {
-	return models['device'].find({ type: req.params.type }).exec()
-	.then(function(devices) {
-		res.json(devices);
-	});
+app.get('/devices/:type', function(req, res) {
+  return models['device'].find({
+      type: req.params.type
+    }).exec()
+    .then(function(devices) {
+      res.json(devices);
+    });
 });
 
 /*
 GET devices/:type/:driver
 -> GET devices/speaker/sonos
 */
-app.get('/devices/:type/:driver', function(req,res) {
-	return models['device'].find({ type: req.params.type, driver: req.params.driver }).exec()
-	.then(function(devices) {
-		res.json(devices);
-	});
+app.get('/devices/:type/:driver', function(req, res) {
+  return models['device'].find({
+      type: req.params.type,
+      driver: req.params.driver
+    }).exec()
+    .then(function(devices) {
+      res.json(devices);
+    });
 });
 
 /*
 GET device/:_id
 -> GET device/abc123
 */
-app.get('/device/:deviceId', function(req,res) {
-	return models['device'].findOne({ _id: req.params.deviceId }).exec()
-	.then(function(device) {
-		res.json(device);
-	});
+app.get('/device/:deviceId', function(req, res) {
+  return models['device'].findOne({
+      _id: req.params.deviceId
+    }).exec()
+    .then(function(device) {
+      res.json(device);
+    });
 });
 
 /*
 GET drivers
 */
-app.get('/drivers', function(req,res) {
-	var devicesGroupedByDrivers = [];
-	models['device'].aggregate([
-        {
-            $group: {
-                _id: '$driver',
-                type: {$first: '$type'},
-                deviceCount: {$sum: 1}
-            }
+app.get('/drivers', function(req, res) {
+  var devicesGroupedByDrivers = [];
+  models['device'].aggregate([{
+      $group: {
+        _id: '$driver',
+        type: {
+          $first: '$type'
+        },
+        deviceCount: {
+          $sum: 1
         }
-    ]).exec()
+      }
+    }]).exec()
     .then(function(results) {
-    	 devicesGroupedByDrivers = results;
+      devicesGroupedByDrivers = results;
 
-    	var promises = [];
-		fs.readdirSync(__dirname + '/drivers').forEach(function(file) {
-	  		if (file.match(/\.js$/) !== null && file !== 'index.js') {
-	    		var name = file.replace('.js', '').replace('homebox-driver-','');
-	    		promises.push(loadDriver(name));
-	  		}
-		});
-		return Promise.all(promises);
-	}).then(function(drivers) {
-		var driversWithStats = _.map(drivers, function(driver){
-			var obj = {
-				_id: driver.name,
-				type: driver.type,
-				deviceCount: 0
-			};
-			var foundStats = _.findWhere(devicesGroupedByDrivers, {_id: driver.name});
-			if(foundStats) {
-				obj.deviceCount = foundStats.deviceCount;
-			}
-			return obj;
-		});
-		res.json(driversWithStats);
+      var promises = [];
+      fs.readdirSync(__dirname + '/drivers').forEach(function(file) {
+        if (file.match(/\.js$/) !== null && file !== 'index.js') {
+          var name = file.replace('.js', '').replace('homebox-driver-', '');
+          promises.push(loadDriver(name));
+        }
+      });
+      return Promise.all(promises);
+    }).then(function(drivers) {
+      var driversWithStats = _.map(drivers, function(driver) {
+        var obj = {
+          _id: driver.name,
+          type: driver.type,
+          deviceCount: 0
+        };
+        var foundStats = _.findWhere(devicesGroupedByDrivers, {
+          _id: driver.name
+        });
+        if (foundStats) {
+          obj.deviceCount = foundStats.deviceCount;
+        }
+        return obj;
+      });
+      res.json(driversWithStats);
     });
 });
 
 app.listen(3000, function() {
-	console.log('homebox listening on port 3000!');
+  console.log('homebox listening on port 3000!');
 });
--- models/speaker.js
+++ models/speaker.js
@@ -3,118 +3,118 @@ var Schema = mongoose.Schema;
 
 
 var SpeakerSchema = new mongoose.Schema({
-  _id: false,
-  name: {
-    type: String,
-    required: true
-  },
-  deviceId: {
-    type: String,
-    required: true
-  },
-  interface: {
-  	type: String,
-  	required: true,
-  	enum: ['wifi']
-  },
-  capabilities: {
-  	getCurrentTrack: {
-		type: Boolean,
-		default: false
+	_id: false,
+	name: {
+		type: String,
+		required: true
 	},
-	getDeviceDescription: {
-		type: Boolean,
-		default: false
+	deviceId: {
+		type: String,
+		required: true
 	},
-	flushQueue: {
-		type: Boolean,
-		default: false
+	interface: {
+		type: String,
+			required: true,
+			enum: ['wifi']
 	},
-	getCurrentState: {
-		type: Boolean,
-		default: false
-	},
-	getLEDState: {
-		type: Boolean,
-		default: false
-	},
-	getMusicLibrary: {
-		type: Boolean,
-		default: false
-	},
-	getMuted: {
-		type: Boolean,
-		default: false
-	},
-	getTopology: {
-		type: Boolean,
-		default: false
-	},
-	getVolume: {
-		type: Boolean,
-		default: false
-	},
-	getZoneAttrs: {
-		type: Boolean,
-		default: false
-	},
-	getZoneInfo: {
-		type: Boolean,
-		default: false
-	},
-	next: {
-		type: Boolean,
-		default: false
-	},
-	pause: {
-		type: Boolean,
-		default: false
-	},
-	play: {
-		type: Boolean,
-		default: false
-	},
-	previous: {
-		type: Boolean,
-		default: false
-	},
-	addToQueueBottom: {
-		type: Boolean,
-		default: false
-	},
-	addToQueueNext: {
-		type: Boolean,
-		default: false
-	},
-	seek: {
-		type: Boolean,
-		default: false
-	},
-	setLEDState: {
-		type: Boolean,
-		default: false
-	},
-	setMuted: {
-		type: Boolean,
-		default: false
-	},
-	setName: {
-		type: Boolean,
-		default: false
-	},
-	setPlayMode: {
-		type: Boolean,
-		default: false
-	},
-	setVolume: {
-		type: Boolean,
-		default: false
-	},
-	stop: {
-		type: Boolean,
-		default: false
+	capabilities: {
+		getCurrentTrack: {
+			type: Boolean,
+			default: false
+		},
+		getDeviceDescription: {
+			type: Boolean,
+			default: false
+		},
+		flushQueue: {
+			type: Boolean,
+			default: false
+		},
+		getCurrentState: {
+			type: Boolean,
+			default: false
+		},
+		getLEDState: {
+			type: Boolean,
+			default: false
+		},
+		getMusicLibrary: {
+			type: Boolean,
+			default: false
+		},
+		getMuted: {
+			type: Boolean,
+			default: false
+		},
+		getTopology: {
+			type: Boolean,
+			default: false
+		},
+		getVolume: {
+			type: Boolean,
+			default: false
+		},
+		getZoneAttrs: {
+			type: Boolean,
+			default: false
+		},
+		getZoneInfo: {
+			type: Boolean,
+			default: false
+		},
+		next: {
+			type: Boolean,
+			default: false
+		},
+		pause: {
+			type: Boolean,
+			default: false
+		},
+		play: {
+			type: Boolean,
+			default: false
+		},
+		previous: {
+			type: Boolean,
+			default: false
+		},
+		addToQueueBottom: {
+			type: Boolean,
+			default: false
+		},
+		addToQueueNext: {
+			type: Boolean,
+			default: false
+		},
+		seek: {
+			type: Boolean,
+			default: false
+		},
+		setLEDState: {
+			type: Boolean,
+			default: false
+		},
+		setMuted: {
+			type: Boolean,
+			default: false
+		},
+		setName: {
+			type: Boolean,
+			default: false
+		},
+		setPlayMode: {
+			type: Boolean,
+			default: false
+		},
+		setVolume: {
+			type: Boolean,
+			default: false
+		},
+		stop: {
+			type: Boolean,
+			default: false
+		}
 	}
-  }
 });
 
 
