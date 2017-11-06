fb0f15a40751d90046c27baf4e7fc251f77b4ae6
Richard Willars <richard@richardwillars.com>
2017-11-06 11:14:51 +0000

    Adds tests for eventCreators

---
+++ eventCreators.test.js
@@ -0,0 +1,25 @@
+const eventCreators = require('./eventCreators');
+
+describe('eventCreators', () => {
+  it('should export a function call deviceEvent', () => {
+    expect(typeof eventCreators.deviceEvent).toEqual('function');
+  });
+
+  it('should map the specified params into an object', () => {
+    const eventId = 'event id';
+    const driverType = 'driver type';
+    const driverId = 'driver id';
+    const deviceId = 'device id';
+    const value = 'value';
+    const result = eventCreators.deviceEvent(eventId, driverType, driverId, deviceId, value);
+    expect(result).toEqual({
+      eventType: 'device',
+      driverType,
+      driverId,
+      deviceId,
+      event: eventId,
+      value,
+    });
+  });
+});
+
