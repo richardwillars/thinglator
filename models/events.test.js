efd18ffd9e48b931c71247ec022defa71ae376dc
Richard Willars <richard@richardwillars.com>
2017-11-15 09:08:16 +0000

    Adds tests for events model

---
+++ models/events.test.js
@@ -0,0 +1,9 @@
+const eventsModule = require('./events');
+const constants = require('../constants');
+
+describe('models/events', () => {
+  it('should export the list of events', () => {
+    const events = eventsModule(constants);
+    expect(events).toMatchSnapshot();
+  });
+});
